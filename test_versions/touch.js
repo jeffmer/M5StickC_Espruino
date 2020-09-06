dff0fc07407f175957ae50fd7c686d629dde8cc6
jeffmer <jeffmer@users.noreply.github.com>
2020-09-06 16:29:20 +0100

    Update touch.js

--- test_versions/touch.js
+++ test_versions/touch.js
@@ -1,11 +1,12 @@
 I2C2.setup({scl:32,sda:23,bitrate:400000});
 
 var TOUCH_PIN = D38;
+pinMode(TOUCH_PIN,'input');
 
 var FT5206 = {
     _data: new Uint8Array(16),
     writeByte:(a,d) => { 
-        I2C2.writeTo(0x35,a,d);
+        I2C2.writeTo(0x38,a,d);
     }, 
     readBytes:(a,n) => {
         I2C2.writeTo(0x38, a);
@@ -19,13 +20,23 @@ var FT5206 = {
         return b>2?0:b;
     },
     getXY:()=>{
-        this._data = FT5206readBytes(0x00,16);
+        this._data = FT5206.readBytes(0x00,16);
         var t = this._data[2];
         if (t>2 || t==0) return;
         return { x:((this._data[3]&0x0F)<<8)|this._data[4],
                  y:((this._data[5]&0x0F)<<8)|this._data[6]
                };
     },
-    sleep:()=>{writeByte(0x87,0x03);},
-    monitor:()=>{writeByte(0x87,0x01);}
+    enable:()=>{FT5206.writeByte(0xA4, 0);}
 };
+
+setWatch(()=> {
+    p = FT5206.getXY();
+    if (p) console.log("touch x: "+p.x+" y:"+p.y);
+    },TOUCH_PIN,{repeat:true,edge:"falling",debounce:25}
+);
+
+FT5206.enable();
+
+
+
